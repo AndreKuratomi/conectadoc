f64fbe09f749367350a08bf97cd011bfb69369ce
André Kuratomi <andrekuratomi@gmail.com>
2022-04-08 19:38:49 -0300

    feat: page home's structure drafted

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,40 @@
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <section>
+          <h2>Bem-vindo à KenzieDoc!</h2>
+          <p>Lorem ipsum dolor sicut amet</p>
+        </section>
+        <section>
+            <article>
+                <h3>LOREM</h3>
+                <p>Lorem ipsum dolor sicut amet</p>
+                <figure>
+                    <img src={} alt=""/>
+                    <figcaption></figcaption>
+                </figure>
+            </article>
+            <article>
+                <h3>LOREM</h3>
+                <p>Lorem ipsum dolor sicut amet</p>
+                <figure>
+                    <img src={} alt=""/>
+                    <figcaption></figcaption>
+                </figure>
+            </article>
+            <article>
+                <h3>LOREM</h3>
+                <p>Lorem ipsum dolor sicut amet</p>
+                <figure>
+                    <img src={} alt=""/>
+                    <figcaption></figcaption>
+                </figure>
+            </article>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+};
