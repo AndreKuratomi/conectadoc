418c7dad5def4f9fcd88818d4a49acc5a1351133
André Kuratomi <andrekuratomi@gmail.com>
2022-04-09 00:34:07 -0300

    feat: Header component drafted

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,26 @@
+import { Link, useNavigate } from "react-router-dom";
+
+export const Header = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <header>
+        <Link to="/">
+          <h1>KenzieDoc</h1>
+        </Link>
+        {auth ? (
+          <UserLogged />
+        ) : (
+          <div>
+            <button onClick={() => navigate.push("/login")}>Login</button>
+            <button onClick={() => navigate.push("/signup")}>
+              Cadastre-se
+            </button>
+          </div>
+        )}
+      </header>
+      {/* fazer lista de headers para ver quando usar cada um? */}
+    </>
+  );
+};
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,3 +1,5 @@
+import { Header } from "../../components/Header";
+
 const Login = () => {
   return (
     <>
